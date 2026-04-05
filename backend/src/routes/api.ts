1277720f3a355926181e3a116f607ba8b777962a
Enes Efe Tokta <enesefetokta009@gmail.com>
2026-04-05 18:18:14 +0300

    feat: add experience management endpoints and validation schemas

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createProject, deleteProject, getAllProjects, getProjectById, updateProject } from "../controllers/project.controller";
 import { createBlogPost, deleteBlogPost, getAllBlogPosts, getBlogPostById, updateBlogPost } from "../controllers/blogpost.controller";
+import { createExperience, deleteExperience, getAllExperiences, getExperienceById, updateExperience } from "../controllers/experience.controller";
 
 const router = Router();
 
@@ -15,5 +16,11 @@ router.get("/blogposts/:id", getBlogPostById);
 router.post("/blogposts", createBlogPost);
 router.put("/blogposts/:id", updateBlogPost);
 router.delete("/blogposts/:id", deleteBlogPost);
+
+router.get("/experiences", getAllExperiences);
+router.get("/experiences/:id", getExperienceById);
+router.post("/experiences", createExperience);
+router.put("/experiences/:id", updateExperience);
+router.delete("/experiences/:id", deleteExperience);
 
 export default router;
